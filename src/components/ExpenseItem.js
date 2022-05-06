c3434e3d3fdc23c4398b769a31d01bc1ea0eca71
Saint-Josh <saintjosh26@gmail.com>
2022-05-06 17:58:31 +0100

    More code added to Expense.js component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function ExpenseItem() {
-  return <h2> Hello from Expense Item</h2>;
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Inusrance</h2>
+        <div>£ 294.67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
